2c942b49b74d761364ff820424d562fd8a03d47e
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-04-05 13:08:41 +0200

    Autoformatting

--- recovery.go
+++ recovery.go
@@ -1,22 +1,22 @@
 // Package recovery implements couple of middleware interfaces
-// 
+//
 // package main
-// 
+//
 // import (
 //     "github.com/vanng822/r2router"
 //     "github.com/vanng822/recovery"
 //     "net/http"
 // )
-// 
+//
 // func main() {
 //     seefor := r2router.NewSeeforRouter()
 //     rec := recovery.NewRecovery()
 //     rec.PrintStack = true
 //     seefor.Before(rec.Handler)
-// 	   seefor.Get("/user/keys/:id", func(w http.ResponseWriter, r *http.Request, _ r2router.Params) {
+// seefor.Get("/user/keys/:id", func(w http.ResponseWriter, r *http.Request, _ r2router.Params) {
 // 	   		panic("This shouldn't crash Seefor")
 // 	   })
-// 	
+//
 //     http.ListenAndServe(":8080", seefor)
 // }
 package recovery
