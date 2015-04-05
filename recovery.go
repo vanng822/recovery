69d8dae2e022613ff8496ad8e5ee7cde0e991ccd
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-04-05 13:12:12 +0200

    Correct docstring this time?!

--- recovery.go
+++ recovery.go
@@ -1,24 +1,24 @@
 // Package recovery implements couple of middleware interfaces
 //
-// package main
+//	package main
 //
-// import (
-//     "github.com/vanng822/r2router"
-//     "github.com/vanng822/recovery"
-//     "net/http"
-// )
+//	import (
+//		"github.com/vanng822/r2router"
+//		"github.com/vanng822/recovery"
+//		"net/http"
+//	)
 //
-// func main() {
-//     seefor := r2router.NewSeeforRouter()
-//     rec := recovery.NewRecovery()
-//     rec.PrintStack = true
-//     seefor.Before(rec.Handler)
-// seefor.Get("/user/keys/:id", func(w http.ResponseWriter, r *http.Request, _ r2router.Params) {
-// 	   		panic("This shouldn't crash Seefor")
-// 	   })
+//	func main() {
+//		seefor := r2router.NewSeeforRouter()
+//		rec := recovery.NewRecovery()
+//		rec.PrintStack = true
+//		seefor.Before(rec.Handler)
+//		seefor.Before(r2router.WrapBeforeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//			panic("Middleware panic")
+//		})))
 //
-//     http.ListenAndServe(":8080", seefor)
-// }
+//		http.ListenAndServe(":8080", seefor)
+//	}
 package recovery
 
 import (
